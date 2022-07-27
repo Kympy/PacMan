9651c5ad01c95c2a0efbbeab253d71a0875f72ac
Parrot_Yong <65384983+Kympy@users.noreply.github.com>
2022-07-27 16:56:02 +0900

    AI 향상

--- Pac_Man/Pac_Man/Enemy.cs
+++ Pac_Man/Pac_Man/Enemy.cs
@@ -15,6 +15,11 @@ namespace Pac_Man
 
         private int distanceX;
         private int distanceY;
+
+        private bool isIsolate = false; // 고립 판단 변수
+        private int solutionDepth = 1; // 솔루션의 깊이
+
+        private int best = 0;
         public Enemy()
         {
             InitPos();
@@ -30,85 +35,46 @@ namespace Pac_Man
             X = random.Next(MaxHeight / 4, MaxHeight / 4 * 3);
             Y = random.Next(MaxWidth / 4, MaxWidth / 4 * 3);
         }
-        public override void Move()
+        public override void Move() // AI 이동
         {
             distanceX = X - GameLoop.Instance.GetPlayer().tileX;
             distanceY = Y - GameLoop.Instance.GetPlayer().tileY;
             if (MathF.Abs(distanceX) + MathF.Abs(distanceY) < 15)
             {
-                if (distanceX > 0) // 플레이어가 더 위에 있다면
+                if(isIsolate) // 플레이어를 추격중인데 고립되었다면
                 {
-                    if (flag) // 애니메이션
-                    {
-                        SetTileShape('∪');
-                    }
-                    else
-                    {
-                        SetTileShape('∨');
-                    }
-                    flag = !flag;
-                    if (GameLoop.Instance.GetRender.GetTile(X - 1, Y).isWall)
-                    {
-                        return; // 벽 못 지나감
-                    }
-                    X -= 1; // 위로 이동
-                    if (X <= 1) X = 1;
+                    IsolatedSolution(X, Y); // 고립 해결 솔루션
+                    return;
                 }
-                else if (distanceX < 0)
+                best = 0;
+                if (MathF.Abs(distanceX) == MathF.Abs(distanceY)) //  XY 거리차의 절댓값 이 같으면
                 {
-                    if (flag)
-                    {
-                        SetTileShape('∩');
-                    }
-                    else
-                    {
-                        SetTileShape('∧');
-                    }
-                    flag = !flag;
-                    if (GameLoop.Instance.GetRender.GetTile(X + 1, Y).isWall)
-                    {
-                        return;
-                    }
-                    X += 1;
-                    if (X >= MaxHeight) X = MaxHeight;
+                    best = random.Next(0, 4); // 아무 방향으로 간다.
                 }
-                else if (distanceY > 0)
+                if (MathF.Abs(distanceX) > MathF.Abs(distanceY)) // X 거리차가 더 크다면
                 {
-                    if (flag)
+                    if (distanceX > 0) // 그 중에서도 양수라면
                     {
-                        SetTileShape('⊃');
+                        best = 0; // 위로 이동
                     }
-                    else
-                    {
-                        SetTileShape('＞');
-                    }
-                    flag = !flag;
-                    if (GameLoop.Instance.GetRender.GetTile(X, Y - 1).isWall)
-                    {
-                        return;
-                    }
-                    Y -= 1;
-                    if (Y <= 1) Y = 1;
+                    else best = 1; // 음수면 아래로 이동
                 }
-                else if (distanceY < 0)
+                else // Y 거리차가 더 크다면
                 {
-                    if (flag)
+                    if (distanceY > 0) // 양수라면
                     {
-                        SetTileShape('⊂');
+                        best = 2; // 왼쪽 이동
                     }
-                    else
-                    {
-                        SetTileShape('＜');
-                    }
-                    flag = !flag;
-                    if (GameLoop.Instance.GetRender.GetTile(X, Y + 1).isWall)
-                    {
-                        return;
-                    }
-                    Y += 1;
-                    if (Y >= MaxWidth) Y = MaxWidth;
+                    else best = 3; // 오른쪽 이동
                 }
-                else return;
+
+                switch(best) // 최적 솔루션
+                {
+                    case 0: { MoveUP(); break; }
+                    case 1: { MoveDown(); break; }
+                    case 2: { MoveLeft(); break; }
+                    case 3: { MoveRight(); break; }
+                }
             }
             else
             {
@@ -211,9 +177,141 @@ namespace Pac_Man
                 }
             }
         }
-        public void FindPlayer()
+        public void IsolatedSolution(int _x, int _y) // 고립 해결 솔루션
         {
-
+            int[] dir = new int[4];
+            if(GameLoop.Instance.GetRender.GetCreator().IsRangeOut(_x - 1, _y) == false ||
+                GameLoop.Instance.GetRender.GetTile(_x - 1, _y).isWall == false) // 위로 갈 수 있으면
+            {
+                dir[0] = 1;
+            }
+            if (GameLoop.Instance.GetRender.GetCreator().IsRangeOut(_x - 1, _y) == false ||
+               GameLoop.Instance.GetRender.GetTile(_x + 1, _y).isWall == false) // 아래로 갈 수 있으면
+            {
+                dir[1] = 1;
+            }
+            if (GameLoop.Instance.GetRender.GetCreator().IsRangeOut(_x - 1, _y) == false ||
+                GameLoop.Instance.GetRender.GetTile(_x , _y - 1).isWall == false) // 왼쪽으로 갈 수 있으면
+            {
+                dir[2] = 1;
+            }
+            if (GameLoop.Instance.GetRender.GetCreator().IsRangeOut(_x - 1, _y) == false ||
+                GameLoop.Instance.GetRender.GetTile(_x, _y + 1).isWall == false) // 오른쪽으로 갈 수 있으면
+            {
+                dir[3] = 1;
+            }
+            int bestWay = 0; int count = 0; // 최적은 
+            for(int i = 0; i < 4; i++)
+            {
+                if (dir[i] == 1) // 최적의 갯수를 세기
+                {
+                    count++;
+                }
+            }
+            if(count > 1) // 만약 최적이 여러개라면
+            {
+                bestWay = random.Next(0, 4); // 랜덤 이동
+            }
+            else // 최적이 1개라면
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (dir[i] == 1) // 
+                    {
+                        bestWay = i; // 최적 경로 저장하고 브레이크
+                        break;
+                    }
+                }
+            }
+            switch(bestWay) // 최적경로에 따라 이동
+            {
+                case 0: { MoveUP(); break; }
+                case 1: { MoveDown(); break; }
+                case 2: { MoveLeft(); break; }
+                case 3: { MoveRight(); break; }
+            }
+            solutionDepth++; // 깊이 1 증가
+            if(solutionDepth > 5) // 1부터 시작해서 5까지 5번 호출동안 솔루션 제공
+            {
+                solutionDepth = 0; // 다시 0으로
+                isIsolate = false; // 고립 해제
+            }
+        }
+        public void MoveUP()
+        {
+            if (flag) // 애니메이션
+            {
+                SetTileShape('∪');
+            }
+            else
+            {
+                SetTileShape('∨');
+            }
+            flag = !flag;
+            if (GameLoop.Instance.GetRender.GetTile(X - 1, Y).isWall)
+            {
+                isIsolate = true;
+                return; // 벽 못 지나감
+            }
+            X -= 1; // 이동
+            if (X <= 1) X = 1;
+        }
+        public void MoveDown()
+        {
+            if (flag)
+            {
+                SetTileShape('∩');
+            }
+            else
+            {
+                SetTileShape('∧');
+            }
+            flag = !flag;
+            if (GameLoop.Instance.GetRender.GetTile(X + 1, Y).isWall)
+            {
+                isIsolate = true;
+                return; // 벽 못 지나감
+            }
+            X += 1;
+            if (X >= MaxHeight) X = MaxHeight;
+        }
+        public void MoveLeft()
+        {
+            if (flag)
+            {
+                SetTileShape('⊃');
+            }
+            else
+            {
+                SetTileShape('＞');
+            }
+            flag = !flag;
+            if (GameLoop.Instance.GetRender.GetTile(X, Y - 1).isWall)
+            {
+                isIsolate = true;
+                return; // 벽 못 지나감
+            }
+            Y -= 1;
+            if (Y <= 1) Y = 1;
+        }
+        public void MoveRight()
+        {
+            if (flag)
+            {
+                SetTileShape('⊂');
+            }
+            else
+            {
+                SetTileShape('＜');
+            }
+            flag = !flag;
+            if (GameLoop.Instance.GetRender.GetTile(X, Y + 1).isWall)
+            {
+                isIsolate = true;
+                return; // 벽 못 지나감
+            }
+            Y += 1;
+            if (Y >= MaxWidth) Y = MaxWidth;
         }
     }
 }
